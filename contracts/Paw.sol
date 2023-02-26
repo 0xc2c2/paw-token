3180d810c77fff5399376afb831010a5a07477c9
0xc2c2 <97208835+0xc2c2@users.noreply.github.com>
2023-02-25 17:46:52 -0800

    change max supply to 10 billion

--- contracts/Paw.sol
+++ contracts/Paw.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract PAW is ERC20
 {
 	constructor() ERC20 ('Paw', 'PAW') {
-		_mint(_msgSender(), 10 ** 12 * 10 ** 18);
+		_mint(_msgSender(), 10 ** 10 * 10 ** 18);
 	}
 }
